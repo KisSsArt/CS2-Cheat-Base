5e1cbdfa91ee24620d1ce9ee15356b51204915f5
artem <artem_kisel04@mail.ru>
2023-04-16 00:39:14 +0300

    Update EngineClient.h

--- Cheat-Base/Source/SDK/Source2/EngineClient.h
+++ Cheat-Base/Source/SDK/Source2/EngineClient.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../VirtualMethod.h"
+#include "../Vector.h"
 
 class EngineClient {
 public:
@@ -12,5 +13,18 @@ public:
     bool isInGame()
     {
         return VirtualMethod::getMethod<bool(__thiscall*)(void*)>(this, 30)(this);
+    }
+
+    void getScreenSize(int& w, int& h)
+    {
+        using getScreenSize_t = void(__thiscall*)(void*, int&, int&);
+        VirtualMethod::getMethod<getScreenSize_t>(this, 48)(this, w, h);
+    }
+
+    Vector getScreenSize()
+    {
+        int w, h;
+        getScreenSize(w, h);
+        return { static_cast<float>(w), static_cast<float>(h), 0.0f };
     }
 };
