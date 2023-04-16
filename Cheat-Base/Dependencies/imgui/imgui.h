3e5dc15aaea09c5d9ec2ca111440cb5c631ca2f2
artem <artem_kisel04@mail.ru>
2023-04-17 00:46:02 +0300

    Update imgui.h

--- Cheat-Base/Dependencies/imgui/imgui.h
+++ Cheat-Base/Dependencies/imgui/imgui.h
@@ -257,6 +257,7 @@ struct ImVec2
     constexpr ImVec2(float _x, float _y)    : x(_x), y(_y) { }
     float  operator[] (size_t idx) const    { IM_ASSERT(idx == 0 || idx == 1); return (&x)[idx]; }  // We very rarely use this [] operator, the assert overhead is fine.
     float& operator[] (size_t idx)          { IM_ASSERT(idx == 0 || idx == 1); return (&x)[idx]; }  // We very rarely use this [] operator, the assert overhead is fine.
+    ImVec2 operator* (float value) { return { x * value, y * value }; }
 #ifdef IM_VEC2_CLASS_EXTRA
     IM_VEC2_CLASS_EXTRA     // Define additional constructors and implicit cast operators in imconfig.h to convert back and forth between your math types and ImVec2.
 #endif
