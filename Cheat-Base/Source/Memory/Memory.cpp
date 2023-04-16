e15ba1197fdb13f3ff74b539e397b7e963c96e11
artem <artem_kisel04@mail.ru>
2023-04-17 00:45:31 +0300

    memory

---
+++ Cheat-Base/Source/Memory/Memory.cpp
@@ -0,0 +1,31 @@
+#include "Memory.h"
+
+#include "../Interfaces.h"
+
+#include "../SDK/Source2/GameResourceService.h"
+
+#include "../SDK/Utils.h"
+
+Memory::Memory() noexcept
+{
+    clientModule.init("client.dll");
+    sdl2.init("SDL2.dll");
+
+    globalVars = clientModule.FindPattern(SIGNATURE("48 8B 05 ? ? ? ? 48 8B D8 80 78 3D 00 75 1D 80 78 3C 00 75")).toAbsolute(3, 7).get<GlobalVars*>();
+    input = clientModule.FindPattern(SIGNATURE("48 8B 0D ? ? ? ? 48 8B 01 FF 50 18 8B DF 66 0F 1F 44 00 ? 48 8B 0D")).toAbsolute(3, 7).get<Input*>();
+    entitySystem = interfaces->gameResourceService->getGameEntitySystem();
+    worldToScreen = clientModule.FindPattern(SIGNATURE("E8 ? ? ? ? F3 0F 10 45 ? 8B D0")).toAbsolute(1, 5).get<decltype(worldToScreen)>();
+
+    createMove = clientModule.FindPattern(SIGNATURE("48 8B C4 48 89 58 10 48 89 48 08 55 56 57 41 54 41 55")).get<std::uintptr_t>();
+    levelInit = clientModule.FindPattern(SIGNATURE("48 89 5C 24 ? 57 48 83 EC 20 48 8B 0D ? ? ? ? 48 8B DA 45 33 C9")).get<std::uintptr_t>();
+    frameStage = clientModule.FindPattern(SIGNATURE("48 89 5C 24 ? 56 48 83 EC 30 8B 05 ? ? ? ? 8D 5A FF 3B C2 48")).get<std::uintptr_t>();
+    mouseInputEnabled = clientModule.FindPattern(SIGNATURE("40 53 48 83 EC 20 80 B9 ? ? ? ? ? 48 8B D9 75 78")).get<std::uintptr_t>();
+
+    getBaseEntity = clientModule.FindPattern(SIGNATURE("8B D3 E8 ? ? ? ? 48 8B F8 48 85 C0 74 76")).toAbsolute(3, 0).get<decltype(getBaseEntity)>();
+    getBaseEntityFromHandle = clientModule.FindPattern(SIGNATURE("E8 ? ? ? ? 4D 8B 3E 4C 89 7C 24 ?")).toAbsolute(1, 0).get<decltype(getBaseEntityFromHandle)>();
+    getHighestEntityIndex = clientModule.FindPattern(SIGNATURE("33 DB E8 ? ? ? ? 8B 08")).toAbsolute(3, 0).get<decltype(getHighestEntityIndex)>();
+
+    SDL_SetRelativeMouseMode = sdl2.GetProcAddress<decltype(SDL_SetRelativeMouseMode)>("SDL_SetRelativeMouseMode");
+    SDL_SetWindowGrab = sdl2.GetProcAddress<decltype(SDL_SetWindowGrab)>("SDL_SetWindowGrab");
+    SDL_WarpMouseInWindow = sdl2.GetProcAddress<decltype(SDL_WarpMouseInWindow)>("SDL_WarpMouseInWindow");
+}
---
+++ Cheat-Base/Source/Memory/Memory.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+
+#include "../SDK/ScanUtils/stb.h"
+#include "../SDK/ScanUtils/Module.h"
+
+#define SIGNATURE(sig) stb::simple_conversion::build<stb::fixed_string{ sig }>::value
+
+struct GlobalVars;
+struct Vector;
+class Input;
+class GameEntitySystem;
+
+class Memory {
+public:
+    Memory() noexcept;
+    CModule clientModule;
+    CModule sdl2;
+
+    GlobalVars* globalVars;
+    Input* input;
+    GameEntitySystem* entitySystem;
+    bool(__fastcall* worldToScreen)(Vector&, Vector&);
+
+    std::uintptr_t createMove;
+    std::uintptr_t levelInit;
+    std::uintptr_t frameStage;
+    std::uintptr_t  mouseInputEnabled;
+
+    void* (__thiscall* getBaseEntity)(void*, int);
+    void* (__thiscall* getBaseEntityFromHandle)(void*, unsigned int);
+    int(__thiscall* getHighestEntityIndex)(void*, int*);
+
+    int(__stdcall* SDL_SetRelativeMouseMode)(int);
+    int(__stdcall* SDL_SetWindowGrab)(void*, int);
+    int(__stdcall* SDL_WarpMouseInWindow)(void*, int, int);
+};
+
+inline std::unique_ptr<Memory> memory;
