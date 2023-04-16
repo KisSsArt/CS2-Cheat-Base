f758947b136f7353655e8da5898accd8e44963d8
artem <artem_kisel04@mail.ru>
2023-04-17 00:45:56 +0300

    misc

---
+++ Cheat-Base/Source/Hacks/Misc.cpp
@@ -0,0 +1,48 @@
+#include "Misc.h"
+
+#include "../Config/Config.h"
+#include "../Memory/Memory.h"
+
+#include "../SDK/Source2/GameEntitySystem.h"
+#include "../SDK/Source2/MoveType.h"
+#include "../SDK/Source2/UserCmd.h"
+#include "../SDK/Source2/CSPlayerPawnBase.h"
+
+void Misc::BunnyHop(UserCmd* cmd)
+{
+    static auto lastJumped = false;
+    static auto shouldJump = false;
+
+    if (!config->misc.bunnyHop)
+        return;
+
+    auto localEntity = memory->entitySystem->getLocalPlayerController();
+    if (!localEntity->pawnIsAlive())
+        return;
+
+    CSPlayerPawnBase* pawn = localEntity->getPawn();
+    if (!pawn)
+        return;
+
+    if (pawn->moveType() == MoveType::MOVETYPE_LADDER)
+        return;
+
+    if (!lastJumped && shouldJump) {
+        shouldJump = false;
+        cmd->buttons |= UserCmd::IN_JUMP;
+    }
+    else if (cmd->buttons & UserCmd::IN_JUMP) {
+        if (pawn->flags() & fl_onground) {
+            lastJumped = true;
+            shouldJump = true;
+        }
+        else {
+            cmd->buttons &= ~UserCmd::IN_JUMP;
+            lastJumped = false;
+        }
+    }
+    else {
+        lastJumped = false;
+        shouldJump = false;
+    }
+}
